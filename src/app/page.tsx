2afad813e8aff2db82150c91bcbb6612494e8a10
Alvaro Rodriguez <alvarord519@gmail.com>
2025-02-28 14:49:33 -0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,101 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useState } from "react";
+import confetti from "canvas-confetti";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [accepted, setAccepted] = useState<boolean>(false);
+  const [swapped, setSwapped] = useState<boolean>(false);
+  const [respuesta, setRespuesta] = useState<string>("No");
+  const posiblesRepuestas: string[] = [
+    "No",
+    "Estas segura?",
+    "Piensalo Bien",
+    "De veritas?",
+    "Ultima chance 🔫",
+    "Si quiero :)",
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const playAudio = () => {
+    const audio = new Audio("/eltajoylatanga.mp3");
+    audio.play().catch((err) => console.log("Play error:", err));
+    setSwapped(true);
+  };
+
+  const checkeandoRespuestas = () => {
+    // When the last answer is reached it will start the function proposalAccepted
+    const index = posiblesRepuestas.indexOf(respuesta);
+    setRespuesta(
+      index === posiblesRepuestas.length - 1
+        ? posiblesRepuestas[0]
+        : posiblesRepuestas[index + 1]
+    );
+
+    if (index === posiblesRepuestas.length - 1) {
+      proposalAccepted();
+    }
+  };
+
+  const proposalAccepted = () => {
+    setAccepted(true);
+    confetti();
+  };
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 text-black bg-white">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div>
+          <div className="w-full flex justify-center mb-10">
+          <p className="text-3xl">ESOOOOO!!</p>
+
+          </div>
+          <img src="/catdancing.gif" alt="cat" className={`${accepted ? '' : 'hidden'} h-82 w-full`} />
+        </div>
+
+        <div className={`${accepted ? "hidden" : ''} `}>
+          <div
+            className={`${
+              swapped ? "mb-5" : "mb-20"
+            } w-full flex justify-center `}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+            <button
+              className={`h-10 border-2 rounded-lg w-1/2 ${
+                swapped ? "hidden" : ""
+              } cursor-pointer`}
+              onClick={playAudio}
+            >
+              PRESIONAMEEE
+            </button>
+            <img
+              src="catdancing.gif"
+              alt="cat-dancing"
+              className={`${swapped ? "" : "hidden"} h-40`}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+
+          <img src="/cat-bowl.gif" alt="gif" />
+
+          <div className="flex flex-col w-full justify-center items-center">
+            <p className="text-black text-2xl">Quieres ir a comer makis? 😁</p>
+
+            <div className="flex gap-4 w-full mt-6">
+              <button
+                className="border rounded-lg h-10 w-full"
+                onClick={proposalAccepted}
+              >
+                Si
+              </button>
+              <button
+                className="border rounded-lg h-10 w-full"
+                onClick={checkeandoRespuestas}
+              >
+                {respuesta}
+              </button>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
